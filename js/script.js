fbf45a2a0c9e58789035db22b9f834742df048d2
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-04-08 16:28:18 +0200

    Trovata soluzione con triplo if + append + classList

--- js/script.js
+++ js/script.js
@@ -12,16 +12,57 @@ const limit = 100
 for (let i = 1; i <= limit; i++) {
     const box = document.createElement('div');
     box.classList.add('box');
-    box.append(i);
     container.append(box);
     
+    // Se i/3 NON ha scarto = fizz
     if (!(i % 3)) {
         box.append('Fizz');
         box.classList.add('fizz');
     }
 
+    // Se i/5 NON ha scarto = buzz
     if (!(i % 5)) {
         box.append('Buzz');
-        box.classList.add('buzz'); 
+        box.classList.add('buzz');
     }
+
+    // Se i/3 e i/5 HANNO entrambi scarto = numeri
+    if ((i % 3 && i % 5)) {
+        box.append(i);
+    }
+
+    /*
+        Visto che ho usato classList le classi si sommano e danno il colore
+        che ho scelto nel CSS per FizzBuzz. Idem per append.
+    */
 };
+
+
+// Come faccio a farlo in altro modo con append?
+/*    
+for (let i = 1; i <= limit; i++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    container.append(box);
+
+    if (!(i % 3)) {
+        box.append('Fizz');
+        box.classList.add('fizz');
+    } else if (!(i % 5)) {
+        box.append('Buzz');
+        box.classList.add('buzz'); 
+    } else if (???) {
+        box.append('FizzBuzz');
+        box.classList.add('fizz');
+        box.classList.add('buzz'); 
+    } else {
+        box.append(i);
+    }
+}
+*/
+
+
+// Come faccio a farlo con innerHTML?
+/*
+
+*/
