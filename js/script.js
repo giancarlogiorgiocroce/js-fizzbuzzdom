bb39ef0c59c642f4e792ff05f187df1d537a7049
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-04-08 16:01:57 +0200

    Milestone 1 (circa)

--- js/script.js
+++ js/script.js
@@ -1,2 +1,27 @@
 /*
+    Stampare in console i numeri da 1 a 100
+    Stampare cose diverse per i multipli di 3, 5 o entrambi
+    Appendere tutto in HTML
 */
+
+// Segno le variabili
+const container = document.querySelector('.container');
+const limit = 100
+
+// Inizializzo il ciclo for
+for (let i = 1; i <= limit; i++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    box.append(i);
+    container.append(box);
+    
+    if (!(i % 3)) {
+        box.append('Fizz');
+        box.classList.add('fizz');
+    }
+
+    if (!(i % 5)) {
+        box.append('Buzz');
+        box.classList.add('buzz'); 
+    }
+};
