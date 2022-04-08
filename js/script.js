e2639baddf6319e594e5fe32a2a24e32e356e71e
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-04-08 17:22:33 +0200

    Versione definitiva

--- js/script.js
+++ js/script.js
@@ -13,37 +13,6 @@ for (let i = 1; i <= limit; i++) {
     const box = document.createElement('div');
     box.classList.add('box');
     container.append(box);
-    
-    // Se i/3 NON ha scarto = fizz
-    if (!(i % 3)) {
-        box.append('Fizz');
-        box.classList.add('fizz');
-    }
-
-    // Se i/5 NON ha scarto = buzz
-    if (!(i % 5)) {
-        box.append('Buzz');
-        box.classList.add('buzz');
-    }
-
-    // Se i/3 e i/5 HANNO entrambi scarto = numeri
-    if ((i % 3 && i % 5)) {
-        box.append(i);
-    }
-
-    /*
-        Visto che ho usato classList le classi si sommano e danno il colore
-        che ho scelto nel CSS per FizzBuzz. Idem per append.
-    */
-};
-
-
-// Come faccio a farlo in altro modo con append?
-/*
-for (let i = 1; i <= limit; i++) {
-    const box = document.createElement('div');
-    box.classList.add('box');
-    container.append(box);
 
     if (!(i % 3) && !(i % 5)) {
         box.append('FizzBuzz');
@@ -59,12 +28,10 @@ for (let i = 1; i <= limit; i++) {
         box.append(i);
     };
 
-    
-    //Dovevo solo posizionare la possibilità doppia più
-    //in alto nella lista delle possibilità in modo da
-    //dargli la precedenza!
+// Le possibilità non si sovrascrivono a vicenda; se voglio
+// che una possibilità "combinata" venga recepita la devo
+// mettere in cima ai casi possibili.
 };
-*/
 
 
 
